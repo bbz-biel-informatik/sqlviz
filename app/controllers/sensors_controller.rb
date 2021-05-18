9c082af8a883358ab338b0481f189e7ab7324fcd
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-05-18 23:09:11 +0200

    store sensor data in db

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -3,6 +3,10 @@ class SensorsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    render plain: 'OK'
+    sensor_name = params[:sensor]
+    sensor_value = params[:value]
+
+    sensor = Sensor.find_or_create_by(name: sensor_name)
+    sensor.values.create(value: sensor_value)
   end
 end
---
+++ db/migrate/20210518210706_create_sensors.rb
@@ -0,0 +1,10 @@
+class CreateSensors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sensors do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
