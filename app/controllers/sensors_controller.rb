86aa1556b9cc5158e59493a8d1009e156809b724
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-05-18 22:18:23 +0200

    render ok on sensor create

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -1,7 +1,8 @@
 class SensorsController < ApplicationController
   skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
 
   def create
-    
+    render plain: 'OK'
   end
 end
