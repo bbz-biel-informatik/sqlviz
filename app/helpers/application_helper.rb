a8738cf1fdf407a86e3dc2607ab6db5b26a91236
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-05-20 21:12:27 +0200

    add legend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def info(article)
     link_to '&#9432;'.html_safe, documentation_path(article), class: 'documentation', target: '_blank'
   end
+
+  def legend(text, index = 0)
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    "<div class='ct-series-#{letters[index]} legend'>" +
+      "<svg viewBox='0 0 100 6' xmlns='http://www.w3.org/2000/svg'>" +
+        "<rect class='ct-line' width='1' height='1' x='2' y='3' />" +
+        "<text class='label' x='6' y='5'>#{text}</text>" +
+      "</svg>" +
+    "</div>"
+  end
 end
